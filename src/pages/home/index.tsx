2359b3368c5bc4660102092f4af79fa3618439f0
Robson <robson.miranda@lunes.io>
2022-05-28 16:24:20 -0300

    feature: configure project setup
    - added eslint
    - added prettier
    - added styled components
    - added react router dom

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import * as Styles from "./styles"
+
+const Home = () => {
+    return (
+        <Styles.Container>
+            <Styles.H1>Hello World</Styles.H1>
+        </Styles.Container>
+    )
+}
+
+export default Home
---
+++ src/pages/home/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+    width: 100%;
+    background-color: #f9f9f9;
+`
+
+export const H1 = styled.h1`
+    color: #0075be;
+    font-size: 2.5rem;
+`
